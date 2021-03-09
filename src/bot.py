182540e358f26edfdd728d9fec5067844fde1387
kanedaaaa <samflynn19121@gmail.com>
2021-03-09 11:36:52 +0100

    add role & presence

--- src/bot.py
+++ src/bot.py
@@ -6,11 +6,9 @@ from discord.ext.commands import CommandNotFound
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=os.environ['PREFIX'], intents=intents)
 
-@bot.event
-async def on_ready():
-    game = discord.Game(f'{bot.command_prefix}help')
-    await bot.change_presence(activity=game)
-    print('Ready!')
+@client.event
+async def on_ready() :
+    await client.change_presence(status = discord.Status.idle, activity = discord.Game("Listening to >help"))
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -21,6 +19,12 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'pong: {round(bot.latency * 1000)}MS')
+
+@client.command()
+async def role(ctx, role: discord.Role):
+  member = ctx.message.author
+  await member.add_roles(role)
+  await ctx.send(f"role granted!")
 
 
 bot.run(os.environ['TOKEN'])
