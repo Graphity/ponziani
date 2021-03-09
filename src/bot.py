d553876065328bcbfb316c83d5ae9ff8489206e4
Graphity <rohood3@gmail.com>
2021-03-09 19:46:44 +0400

    add permission check

--- src/bot.py
+++ src/bot.py
@@ -1,10 +1,11 @@
 import os
 import discord
 from discord.ext import commands
-from discord.ext.commands import CommandNotFound
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix=os.environ['PREFIX'], intents=intents)
+bot = commands.Bot(command_prefix=os.environ["PREFIX"], intents=intents)
+moderator_role_id = int(os.environ["MODERATOR_ROLE_ID"])
+admin_role_id = int(os.environ["ADMIN_ROLE_ID"])
 
 @client.event
 async def on_ready() :
@@ -12,19 +13,24 @@ async def on_ready() :
 
 @bot.event
 async def on_command_error(ctx, error):
-    if isinstance(error, CommandNotFound):
-        await ctx.send('მსგავსი ბრძანებისთვის ლოგიკა არ დაუწერია ჯერ არავის')
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("მსგავსი ბრძანებისთვის ლოგიკა არ დაუწერია ჯერ არავის")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("არგუმენტ(ებ)ი არასწორია")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("Sike!")
     raise error
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f'pong: {round(bot.latency * 1000)}MS')
+    await ctx.send(f"pong: {round(bot.latency * 1000)}MS")
 
 @bot.command()
+@commands.has_any_role(moderator_role_id, admin_role_id)
 async def role(ctx, role: discord.Role):
     member = ctx.message.author
     await member.add_roles(role)
     await ctx.send("role granted!")
 
 
-bot.run(os.environ['TOKEN'])
+bot.run(os.environ["TOKEN"])
