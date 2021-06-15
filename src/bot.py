a7759f73463510b52cf8d16436156fe16b87604f
vipshainidze <david.shainidze.1@btu.edu.ge>
2021-06-16 00:23:13 +0400

    :tangerine: დავამატე administer კოგი. შევცვალე bot ფაილში ერორ ჰენდლინგის ტექსტები - embed-ები დავამატე.

--- src/bot.py
+++ src/bot.py
@@ -8,15 +8,35 @@ moderator_role_id = int(os.environ["MODERATOR_ROLE_ID"])
 admin_role_id = int(os.environ["ADMIN_ROLE_ID"])
 
 
+def generate_mbed_context(title: str, description: str, colour: discord.colour.Colour) -> discord.embeds.Embed:
+    mbed = discord.Embed(title=title,
+                         description=description,
+                         colour=colour)
+    return mbed
+
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("მსგავსი ბრძანებისთვის ლოგიკა არ დაუწერია ჯერ არავის")
-    elif isinstance(error, commands.BadArgument):
-        await ctx.send("არგუმენტ(ებ)ი არასწორია")
+        await ctx.send(embed=generate_mbed_context(
+            title="ბრძანება",
+            description="```მსგავსი ბრძანებისთვის ლოგიკა არ დაუწერია ჯერ არავის.```",
+            colour=discord.Colour.dark_red()
+        ))
+    elif isinstance(error, commands.BadArgument) or isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(embed=generate_mbed_context(
+            title="არგუმენტები",
+            description="```არგუმენტ(ებ)ი არასწორია```",
+            colour=discord.Colour.dark_red()
+        ))
     elif isinstance(error, commands.MissingPermissions):
-        await ctx.send("Sike!")
+        await ctx.send(embed=generate_mbed_context(
+            title="უფლებები",
+            description="```თქვენ არ გაქვთ ამ ბრძანებით სარგებლობის უფლება.```",
+            colour=discord.Colour.dark_red()
+        ))
     raise error
+
 
 @bot.command()
 async def ping(ctx):
@@ -26,6 +46,5 @@ async def ping(ctx):
 for filename in os.listdir("./src/cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
-
 
 bot.run(os.environ["TOKEN"])
---
+++ src/cogs/administer.py
@@ -0,0 +1,111 @@
+import os
+
+import discord
+from discord.ext import commands
+
+
+class Administer(commands.Cog):
+
+    def __init__(self, bot):
+        """
+        Init
+        :param bot: Runner Class
+        """
+        self.client = bot
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int) -> None:
+        """
+        შლის შეტყობინებების კონკრეტულ რაოდენობას.
+        """
+        await ctx.channel.purge(limit=amount)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, member: discord.Member, *, reason=None) -> None:
+        """
+        ბანი.
+        """
+        mbed = discord.Embed(
+            title="Success",
+            description=f"ბანი დაედო მომხმარებელს: {member.display_name}",
+        )
+        await ctx.send(embed=mbed)
+        await member.ban(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: discord.Member, *, reason=None) -> None:
+        """
+        გაგდება.
+        """
+        await member.kick(reason=reason)
+
+    @commands.command()
+    @commands.has_role(int(os.environ["OWNER_ROLE_ID"]))
+    async def load(self, ctx, extension) -> None:
+        """
+        კოგის ინსტალაცია.
+        """
+        self.client.load_extension(f"cogs.{extension}")
+        mbed = discord.Embed(title="COGS",
+                             description=f"Loaded cog: {extension}.",
+                             colour=discord.Colour.dark_red())
+        await ctx.send(embed=mbed)
+
+    @commands.command()
+    @commands.has_role(int(os.environ["OWNER_ROLE_ID"]))
+    async def unload(self, ctx, extension) -> None:
+        """
+        კოგის დეინსტალაცია.
+        """
+        self.client.unload_extension(f"cogs.{extension}")
+        mbed = discord.Embed(title="COGS",
+                             description=f"```Unloaded cog: {extension}```",
+                             colour=discord.Colour.dark_red())
+        await ctx.send(embed=mbed)
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # TODO ERROR HANDLING METHODS BELOW                 #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    @staticmethod
+    def generate_mbed_context(title: str, description: str, colour: discord.colour.Colour) -> discord.embeds.Embed:
+        mbed = discord.Embed(title=title,
+                             description=description,
+                             colour=colour)
+        return mbed
+
+    # @clear.error
+    # async def clear_error(self, ctx, error):
+    #     if isinstance(error, commands.BadArgument):
+    #         mbed = discord.Embed(title="ბრძანებსი არასწორი სინტაქსი",
+    #                              description="```$clear <რაოდენობა>```",
+    #                              colour=discord.Colour.dark_red())
+    #         await ctx.send(embed=mbed)
+
+    @load.error
+    async def load_error(self, ctx, error):
+        if isinstance(error, commands.errors.CommandInvokeError):
+            mbed = self.generate_mbed_context(
+                title="COGS",
+                description="```ინსტალაცია ვერ ხერხდება: უკვე დაინსტალირებულია.```",
+                colour=discord.Colour.dark_red())
+
+            await ctx.send(embed=mbed)
+
+    @unload.error
+    async def unload_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            mbed = self.generate_mbed_context(
+                title="COGS",
+                description="```დეინსტალაცია ვერ ხერხდება: უკვე დეინსტალირებულია.```",
+                colour=discord.Colour.dark_red())
+            await ctx.send(embed=mbed)
+
+
+def setup(bot) -> None:
+    """
+    Cog setup.
+    """
+    bot.add_cog(Administer(bot))
